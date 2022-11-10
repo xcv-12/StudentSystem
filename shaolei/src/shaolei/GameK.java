4263297a67d8a7dc1ab387040393a2eff81ac711
xcv-12 <2253388676@qq.com>
2022-11-10 16:02:55 +0800

    Initial commit

---
+++ shaolei/src/shaolei/GameK.java
@@ -0,0 +1,24 @@
+package shaolei;
+
+
+
+
+public class Game extends Application{
+	
+	 private final String low="低级(8*8,10个雷)";
+	 private final String center="中级(12*12,20个雷)";
+	 private final String high="高级(16*16,40个雷)";
+	 private int mines=0;//雷数
+   private int mark=0;//标记数
+   private int click=0;
+   private int  btnrow=0;
+   private int btncol=0;
+
+
+
+
+
+
+public class GameK {
+
+}
